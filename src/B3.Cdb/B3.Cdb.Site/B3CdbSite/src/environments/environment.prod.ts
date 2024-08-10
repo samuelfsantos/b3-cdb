41f63405b2e2ca141b9ce7c1a6982fb4b1ec5950
Samuel Ferreira Santos <fs.samuel@outlook.com>
2024-08-10 18:06:49 -0300

    ajustes frontend dockerfile e variáveis

--- src/B3.Cdb/B3.Cdb.Site/B3CdbSite/src/environments/environment.prod.ts
+++ src/B3.Cdb/B3.Cdb.Site/B3CdbSite/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.prod.domain.com/api/v1/investmentos/cdb'
+  apiUrl: 'https://b3-cdb-api-fhbcapeadccdfmbc.eastus-01.azurewebsites.net/api/v1/investmentos/cdb'
 };
